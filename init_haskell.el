41ca022fb414e94cfaa6a243251dcaeaf0b24019
Gonzalo Rivero <griverorz@gmail.com>
2014-04-20 20:14:28 -0700

    Added rest of the files

---
+++ init_haskell.el
@@ -0,0 +1,16 @@
+;; Haskell mode
+(autoload 'haskell-mode "haskell-mode.el" "Major mode for editing Haskell files" t)
+
+(require 'haskell-mode-autoloads)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+
+;; Tidal
+(require 'package)
+(add-to-list 'package-archives 
+   '("marmalade" .
+     "http://marmalade-repo.org/packages/"))
+(package-initialize)
+(setq load-path (cons "~/.emacs.d/tidal/" load-path))
+;; (require 'tidal)
+;; (setq tidal-interpreter "/usr/local/bin/ghci")
