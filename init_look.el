c6a9d7b25caae0c46fd2f9c96bf763e3259112f1
Gonzalo Rivero <griverorz@gmail.com>
2015-01-21 14:41:21 -0800

    Updated themes

--- init_look.el
+++ init_look.el
@@ -3,15 +3,15 @@
 
 ;; Theme
 (require 'color-theme)
-(require 'color-theme-solarized)
 (color-theme-initialize)
-(setq solarized-termcolors '256)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'monokai)
+;; (setq solarized-termcolors '256)
 
 ;; set dark theme
-(color-theme-solarized-dark)
-(setq solarized-use-terminal-theme t)
+;; (color-theme-solarized-dark)
+;; (setq solarized-use-terminal-theme t)
 ;; set light theme
-
 
 ;; Color cursor
 (set-cursor-color "#ff0000") 		
