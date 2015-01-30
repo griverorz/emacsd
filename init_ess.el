862b3bd1737b1898127acc3453a8bd6abe27ef35
Gonzalo Rivero <griverorz@gmail.com>
2015-01-30 09:49:21 -0800

    No load data

--- init_ess.el
+++ init_ess.el
@@ -3,6 +3,9 @@
 (require 'ess-eldoc)
 (ess-toggle-underscore nil)
 
+;; Do not load data or save envir
+(setq inferior-R-args "--no-restore-history --no-restore-data --no-save ")
+
 ;; Not to indent comments in R mode
 (add-hook 'ess-mode-hook
           (lambda ()
@@ -20,7 +23,9 @@
 ;; Speedbar
 ;; (speedbar-add-supported-extension ".R")
 
-
+;; Stop doing weird things with the alignment of formulas
+;; (add-hook 'ess-mode-hook (lambda () (setq ess-arg-function-offset 4)))
+;; (add-hook 'ess-mode-hook (lambda () (setq ess-continued-statement-offset 0)))
 
 ;; I don't like if another frame pops up showing the inferior-ess buffer,
 ;; rather I want unconditionally that the buffer is shown in the current frame.
