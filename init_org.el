cf4902e34d963f99865a93e8b48e8a3d751c89d0
Gonzalo Rivero <griverorz@gmail.com>
2015-02-05 19:55:19 -0800

    Local org

---
+++ init_org.el
@@ -0,0 +1,64 @@
+;; Basic configuration
+(require 'org)
+(setq org-directory "~/Documents/org/")
+
+(setq org-log-done t)
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;; Clocking
+(setq org-clock-persist 'history)
+(org-clock-persistence-insinuate)
+
+(setq org-log-done t)
+
+;; Tags shortcuts
+(setq org-tag-alist '(("@work" . ?w) 
+                      ("@home" . ?h) 
+                      ("@research" . ?r)
+                      ("@errands" . ?e) 
+                      ("@coding" . ?c)
+                      ("@email" . ?m)
+                      ("@reading" . ?b)
+                      ("@computer" . ?l)))
+
+;; Set org files
+(setq org-agenda-files (list (concat org-directory "home.org")
+                             (concat org-directory "notes.org")
+                             (concat org-directory "work.org")
+                             (concat org-directory "research.org")))
+
+;; Capture 
+(define-key global-map "\C-cc" 'org-capture)
+(setq org-default-notes-file (concat org-directory "notes.org"))
+
+(setq org-capture-templates 
+      '(
+        ("t" "Todo" entry (file+headline (concat org-directory "notes.org") "Tasks") "* TODO %?\n  %i\n")
+        ("j" "Journal" plain (file+datetree (concat org-directory "journal.org")) "%?\nEntered on %U\n")))
+
+;; Visualization
+(add-hook 'org-mode-hook 
+          (lambda ()
+            (set-face-attribute 'org-level-1 nil :height 1.4)
+            (set-face-attribute 'org-level-2 nil :height 1.2)
+            (set-face-attribute 'org-level-3 nil :height 1.1)
+            (set-face-attribute 'org-level-4 nil :height 1.1)
+            (set-face-attribute 'org-level-5 nil :height 1.1)))
+
+
+;; Indentation in org-mode
+(setq org-startup-indented t)
+
+;; Faces in Org-mode
+(add-hook 'org-mode-hook 
+          (lambda ()
+            (set-face-attribute 'org-level-1 nil :height 1.5)
+            (set-face-attribute 'org-level-2 nil :height 1.2)
+            (set-face-attribute 'org-level-3 nil :height 1.1)
+            (set-face-attribute 'org-level-4 nil :height 1.1)
+            (set-face-attribute 'org-level-5 nil :height 1.1)))
+
