02bc28e20046c67c736ed875b2de7b2f8f3c7be4
Gonzalo Rivero <griverorz@gmail.com>
2016-08-04 18:54:13 -0400

    Simple prompt for ipy

--- init.el
+++ init.el
@@ -171,18 +171,3 @@
 
 (put 'narrow-to-region 'disabled nil)
 (setq-default indent-tabs-mode nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(LaTeX-command "latex -synctex=1")
- '(package-selected-packages
-   (quote
-    (flymake-json magit color-theme-tangotango color-theme-zenburn color-theme-wombat zenburn-theme virtualenvwrapper use-package twittering-mode switch-window smex smart-mode-line-powerline-theme simpleclip python-mode pymacs pycomplete projectile project-root pretty-lambdada pandoc-mode multiple-cursors multi-term material-theme markdown-mode jedi ipython idomenu ido-ubiquitous helm haskell-mode guide-key flymake-python-pyflakes flycheck expand-region ess erc-hipchatify elpy el-get color-theme-solarized autopair auto-virtualenv auto-compile auctex))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "#99968b" :slant italic)))))
