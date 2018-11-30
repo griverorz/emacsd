288ae258cd9132f30db1b6038adb4b1a4c85a5b0
Gonzalo Rivero <gonzalorivero@air.local>
2018-11-30 09:41:45 -0500

    Fixed key for org-mode

--- init-org.el
+++ init-org.el
@@ -23,7 +23,7 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key "\C-cb" 'org-switchb)
 
 ;; Modules
 (setq org-expiry-inactive-timestamps t)
