3f6b34d4a8a13223463d1390a2b93871d017b62a
Gonzalo Rivero <gonzalorivero@westat.com>
2020-10-01 07:32:51 -0400

    Prevents flycheck from opening new window

--- init.el
+++ init.el
@@ -92,12 +92,26 @@
 (setq load-prefer-newer t)
 
 ;; Flycheck diagnostic at point
+(defun flycheck-display-error-messages-truncated (errors)
+  (when (and errors (flycheck-may-use-echo-area-p))
+    (let ((messages (seq-map #'flycheck-error-format-message-and-id errors)))
+      (message (string-join messages "\n\n") ;; here is the relevant modification
+               flycheck-error-message-buffer)
+      ;; We cannot rely on `display-message-or-buffer' returning the right
+      ;; window. See URL `https://github.com/flycheck/flycheck/issues/1643'.
+      (-when-let ((buf (get-buffer flycheck-error-message-buffer)))
+        (with-current-buffer buf
+          (unless (derived-mode-p 'flycheck-error-message-mode)
+            (flycheck-error-message-mode)))))))
+
 (use-package flycheck
   :ensure t
   :config
+  :init (global-flycheck-mode)
   (flymake-mode nil)
-  (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
-  (add-hook 'after-init-hook #'global-flycheck-mode))
+  (setq flycheck-check-syntax-automatically '(save))
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-truncated)
+  (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake))
 
 ;; Load init files
 (load "~/.emacs.d/init-server.el")
